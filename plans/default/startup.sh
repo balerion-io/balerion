f199bc10ecce509d4454172a32ebc25c0bf5918a
Lior Kamrat <likamrat@My-MacBook.local>
2017-09-04 05:57:03 +0300

    test merge

--- plans/default/startup.sh
+++ plans/default/startup.sh
@@ -9,3 +9,5 @@ sed -i "s|myclientsecret|$client_secret|g" /tmp/tfdeployment/secret.tfvars
 # Replace Terraform plan environment variables
 
 sed -i "s| myresourcegroup|$my_resource_group|g" /tmp/tfdeployment/vars.tf
+
+Testing merge
